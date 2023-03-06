8e817840b7be70dfd96966510067af8049afdc35
JUAN DAVID RUIZ OLMOS <juanruiz309381@correo.itm.edu.co>
2023-03-06 10:18:36 -0500

    first commit2

---
+++ WebApp_OOPConcepts/WebApp_OOPConcepts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApp_OOPConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
